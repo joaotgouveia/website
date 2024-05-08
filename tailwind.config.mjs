6bd54485a721973a6b099a3b619978807f7a6afc
João Gouveia <joaotalonegouveia@tecnico.ulisboa.pt>
2024-05-08 15:21:06 +0100

    feat: define theme related css variables

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
+    theme: {
+        extend: {
+           colors: {
+               "bg-primary": "var(--bg-primary)",
+               "bg-secondary": "var(--bg-secondary)",
+               "bg-accent": "var(--bg-accent)",
+               "fg-primary": "var(--fg-primary)",
+               "fg-secondary": "var(--fg-secondary)",
+               "fg-accent": "var(--fg-accent)",
+           },
+        },
+    },
 	plugins: [],
 }
