453a3e600acdd006131bf0dffba3b7aa143efc69
João Gouveia <joaotalonegouveia@tecnico.ulisboa.pt>
2024-05-27 18:01:08 +0100

    fix: image paths

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,6 @@
 import rss from '@astrojs/rss';
 import sanitizeHtml from 'sanitize-html';
+import { parse as htmlParser } from "node-html-parser";
 
 export async function GET(context) {
     const posts = import.meta.glob('@/content/blog/*.md', { eager: true });
@@ -8,6 +9,14 @@ export async function GET(context) {
     for (const key in posts) {
         const post = posts[key];
         const postUrl = context.site + key.replace('/src/content/', '').replace('.md', '');
+        const postBody = post.compiledContent();
+        const postHtml = htmlParser.parse(postBody);
+        const postImages = postHtml.querySelectorAll("img");
+
+        for (const image of postImages) {
+            const src = image.getAttribute("src");
+            image.setAttribute("src", context.site + src.replace("/", ""),);
+        }
 
         feed.push({
             link: postUrl,
@@ -16,7 +25,7 @@ export async function GET(context) {
             author: post.frontmatter.author,
             pubDate: post.frontmatter.date,
             categories: post.frontmatter.tags,
-            content: sanitizeHtml(post.compiledContent(), {
+            content: sanitizeHtml(postHtml.toString(), {
                 allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
             }),
         });
--- src/pages/tags/[tag].rss.xml.js
+++ src/pages/tags/[tag].rss.xml.js
@@ -1,6 +1,7 @@
 import rss from '@astrojs/rss';
 import { getCollection } from "astro:content";
 import sanitizeHtml from 'sanitize-html';
+import { parse as htmlParser } from "node-html-parser";
 
 export async function getStaticPaths() {
     const posts = await getCollection("blog");
@@ -26,6 +27,15 @@ export async function GET(context) {
         const post = posts[key];
         if (post.frontmatter.tags.map(tag => tag.toLowerCase().replaceAll(' ', '-')).includes(params.tag)) {
             const postUrl = context.site + key.replace('/src/content/', '').replace('.md', '');
+            const postBody = post.compiledContent();
+            const postHtml = htmlParser.parse(postBody);
+            const postImages = postHtml.querySelectorAll("img");
+
+            for (const image of postImages) {
+                const src = image.getAttribute("src");
+                image.setAttribute("src", context.site + src.replace("/", ""),);
+            }
+
             feed.push({
                 link: postUrl,
                 title: post.frontmatter.title,
@@ -33,7 +43,7 @@ export async function GET(context) {
                 author: post.frontmatter.author,
                 pubDate: post.frontmatter.date,
                 categories: post.frontmatter.tags,
-                content: sanitizeHtml(post.compiledContent(), {
+                content: sanitizeHtml(postHtml.toString(), {
                     allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
                 }),
             });
